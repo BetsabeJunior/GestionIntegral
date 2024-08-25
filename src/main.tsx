de59534979629476511978a98020b8bc0c3f684e
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2024-08-25 18:02:37 -0500

    Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './routes/Login.tsx'
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element : <Login/>,
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
